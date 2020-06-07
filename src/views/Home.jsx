fdbe231d1bd3dba545f8fbe589212df4e61afe1e
FACEPrep-ProGrad <prograd@faceprep.in>
2020-06-07 16:19:02 +0530

    second

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,11 @@ import introImage from './introImage.png';
 import "./Home.css";
 import Group15 from './Group15.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faUser, faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faChalkboard } from "@fortawesome/free-solid-svg-icons";
+import sideImage from './sideImage.png';
+import Why from './Why.png';
+import Rectangle from './Rectangle.png';
 
 
 const handler = new APIHandler();
@@ -41,22 +45,60 @@ export default function Home() {
         </div>
         <div class="cards">
           <div class="card">
-            <FontAwesomeIcon icon={faUser} />
-            <h4>Sign up</h4>
-            <p>Sign up for free as a school or an instituition and get access to Schoology</p>
+            <FontAwesomeIcon class="cardicon" icon={faUser} />
+            <h3>Sign up</h3>
+            <p>Sign up for free as a school <br></br >or an instituition and get  <br></br> access to Schoology</p>
           </div>
           <div class="card">
-            <FontAwesomeIcon icon={faUser} />
-            <h4>Sign up</h4>
-            <p>Sign up for free as a school or an instituition and get access to Schoology</p>
+            <FontAwesomeIcon class="cardicon" icon={faChalkboard} />
+            <h3>Set up classroom</h3>
+            <p>Invite teachers, students and <br></br> their parents to the <br></br> classroom</p>
           </div>
           <div class="card">
-            <FontAwesomeIcon icon={faUser} />
-            <h4>Sign up</h4>
-            <p>Sign up for free as a school or an instituition and get access to Schoology</p>
+            <FontAwesomeIcon class="cardicon" icon={faEnvelope} />
+            <h3>Set up classroom</h3>
+            <p>Invite teachers, students and <br></br> their parents to the  <br></br> classroom</p>
           </div>
           </div>
       </section>
+      <section id="teachers">
+        <div>
+        <img class="sideimg" src={sideImage} alt="image"/>
+        </div>
+        <div class="part2">
+          <h1>Open Up a Window of Possibilities <br/> for Teachers</h1>
+          <p>The truth is you not only make parent-teacher communication seamless,<br/> 
+            but you also open up a window of possibilities for teacher</p>
+          <ul>
+          <li>Reach any parent any time with instant messaging</li>
+          <li>Parents get to stay in the know of their kid’s education, making <br/> them accountable</li>
+          <li>Schedule meetings with parents anytime anywhere</li>
+          <li>Save real time - No more emails, school diaries and phone calls. </li>
+          </ul>
+        </div>
+      </section>
+      <section id="id">
+        <h2>Why Schools love Schoology</h2>
+        <img class="why" src={Why} alt="image"/>
+        <img class="rectangle" src={Rectangle} alt="image"/>
+        <div class="number">
+            <p class="label1">
+              <label>26000+</label>
+              &nbsp; &nbsp; &nbsp; &nbsp;
+              <label>1600+</label>
+              &nbsp; &nbsp; &nbsp; &nbsp;
+              <label>900+</label>
+            </p>
+            <p class="label2">
+              <label class="lab1">Students </label>
+              <label class="lab2">Teachers</label>
+              <label class="lab3">Schools</label>
+            </p>
+        </div>
+      </section>
+      <div class="footer">
+        <p>Get in touch with us @ 9999999999</p>
+      </div>
     </div>
   );
 }
